79947a6439cc0202052c4a144ea6a39a6e249241
madhav-bhalani <madhavbhalani@gmail.com>
2025-03-30 10:48:43 +0530

    modified for login functionality

--- index.js
+++ index.js
@@ -1,140 +1,142 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const path = require('path');
-const Product = require('./models/products')
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bcrypt = require('bcrypt');
-const User = require('./models/users')
+const path = require("path");
+const Product = require("./models/products");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const User = require("./models/users");
 
-const jwt = require('jsonwebtoken');
-const key = 'santaClause90*32@@';
+const jwt = require("jsonwebtoken");
+const key = "santaClause90*32@@";
 
+const corsOptions = {
+  origin: 'http://localhost:5173', // Replace with your specific origin
+  credentials: true
+  // optionsSuccessStatus: 200
+};
 
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+// app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-mongoose.connect('mongodb://127.0.0.1:27017/vital-gear')
-.then(()=>{
+mongoose
+  .connect("mongodb://127.0.0.1:27017/vital-gear")
+  .then(() => {
     console.log("Connection Open...");
-})
-.catch(err => {
+  })
+  .catch((err) => {
     console.log("Ohhhh noooo errorrrr");
     console.log(err);
-});
+  });
 
 //display Products
-app.get('/displayProducts/:ctg', async(req,res)=>{
-    try{
-        const category = req.params.ctg;
-        const products = await Product.find({category: category});
-        res.json(products);
-    }
-    catch(err){
-        console.log('ERROR IN DISPLAYING PRODUCTS')
-        console.log(err);
-    }
-})
-
-
-
+app.get("/displayProducts/:ctg", async (req, res) => {
+  try {
+    const category = req.params.ctg;
+    const products = await Product.find({ category: category });
+    res.json(products);
+  } catch (err) {
+    console.log("ERROR IN DISPLAYING PRODUCTS");
+    console.log(err);
+  }
+});
 
 //search route
-app.get('/search', (req,res)=>{
-    res.render('search');
+app.get("/search", (req, res) => {
+  res.render("search");
 });
 
-app.post('/searchResults', async(req,res)=>{
-    try{
-        const search = req.body.name;
-        const results = await Product.find({$or: [{productName: {$regex: new RegExp(search, 'i')}}, {brandName: {$regex: new RegExp(search, 'i')}}, {category: search}]});
-        console.log(results);
-        res.render('searchResults', {results});
-    }
-    catch(err){
-        console.log("Error while searching: ");
-        console.log(err);
-    }
-
-  
+app.post("/searchResults", async (req, res) => {
+  try {
+    const search = req.body.name;
+    const results = await Product.find({
+      $or: [
+        { productName: { $regex: new RegExp(search, "i") } },
+        { brandName: { $regex: new RegExp(search, "i") } },
+        { category: search },
+      ],
+    });
+    console.log(results);
+    res.render("searchResults", { results });
+  } catch (err) {
+    console.log("Error while searching: ");
+    console.log(err);
+  }
 });
-
 
 //REGISTER ROUTE
-app.post('/register', async(req,res)=>{
-    try{
-        const {fname, lname, email, phone, password} = req.body;
-        const hash = await bcrypt.hash(password, 12);
-        const user = await User.findOne({$or: [
-            {email: email},
-            {phone: phone}
-        ]});
-        if(user){
-            console.log('User already exists');
-        }
-        else{
-            const newUser =  new User({firstName: fname, lastName: lname, email: email, phone: phone, password: hash});
-            console.log(newUser);
-            await newUser.save();
-            // res.send('User added');
-            res.redirect('http://localhost:5173/');
-        }
+app.post("/register", async (req, res) => {
+  try {
+    const { fname, lname, email, phone, password } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = await User.findOne({
+      $or: [{ email: email }, { phone: phone }],
+    });
+    if (user) {
+      console.log("User already exists");
+    } else {
+      const newUser = new User({
+        firstName: fname,
+        lastName: lname,
+        email: email,
+        phone: phone,
+        password: hash,
+      });
+      console.log(newUser);
+      await newUser.save();
+      // res.send('User added');
+      res.redirect("http://localhost:5173/");
     }
-    catch(err){
-        console.log('Error while registering user');
-        console.log(err);
-    }
+  } catch (err) {
+    console.log("Error while registering user");
+    console.log(err);
+  }
 });
 
-
 //LOGIN ROUTE
-app.post('/login', async(req,res)=>{
-    try{
-        const{username, password} = req.body;
-        const user = await User.findOne({$or: [
-            {phone: username},
-            {email: username}
-        ]});  
-        if(!user){
-            res.send('Please create an account to login');
-        }  
-        else{
-            const verifyPass = await bcrypt.compare(password, user.password);
-            if(verifyPass){
-                jwt.sign({user},key, {expiresIn: '1d'}, (err, token)=>{
-                    if(err){
-                        return res.status(500).send('something went wrong!!');
-                    }
-                    res.cookie('auth', token, {
-                        httpOnly: true,
-                        secure: false,
-                        sameSite: 'Lax',
-                        // expires: new Date(Date.now() * 50000)
-                    });
-                    res.json({message: 'Logged in!!', token, user});
-                });
-            }
-            else{
-                res.send('please enter a correct password');
-            }
-        }
-        
+app.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    console.log("HG", username, password);
+    const user = await User.findOne({
+      $or: [{ phone: username }, { email: username }],
+    });
+    console.log(user);
+    if (!user) {
+      res.send("Please create an account to login");
+    } else {
+      console.log(req.body);
+      const verifyPass = await bcrypt.compare(password, user.password);
+      if (verifyPass) {
+        jwt.sign({ user }, key, { expiresIn: "1d" }, (err, token) => {
+          if (err) {
+            return res.status(500).send("something went wrong!!");
+          }
+          res.cookie("auth", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "Lax",
+            // expires: new Date(Date.now() * 50000)
+          });
+          res.json({ message: "Logged in!!", token, user });
+        });
+      } else {
+        res.send("please enter a correct password");
+      }
     }
-    catch(err){
-        console.log('Error while logging in');
-        console.log(err);
-    }
+  } catch (err) {
+    console.log("Error while logging in");
+    console.log(err);
+  }
 });
 
 //LOGOUT ROUTE
 
- 
-
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- vital.js
+++ vital.js
@@ -1,330 +1,346 @@
-const mongoose = require('mongoose');
-const vitalProduct = require('./models/products');
+const mongoose = require("mongoose");
+const vitalProduct = require("./models/products");
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/vital-gear')
-.then(()=>{
+mongoose
+  .connect("mongodb://127.0.0.1:27017/vital-gear")
+  .then(() => {
     console.log("Connection Open...");
-})
-.catch(err => {
+  })
+  .catch((err) => {
     console.log("Ohhhh noooo errorrrr");
     console.log(err);
-});
+  });
 
 seedProducts = [
-    {
-        productName: "Bizoyme Performance Whey",
-        brandName: "Muscle Blaze",
-        category: "protein",
-        productDetails: {
-            flavours: ["chocolate", "mango"]
-        },
-        sizes: {
-            weight: [500, 1000]
-        },
-        stock: {
-            quantity: 4
-        },
-        price: {
-            productPrice: 2500,
-            onSale: true
-        },
-        images: {
-            displayImage: "/products/protein.jpg",
-            sliderImages: ["/products/protein.jpg", "/products/gainer.webp"]
-        }
+  {
+    productName: "Bizoyme Performance Whey",
+    brandName: "Muscle Blaze",
+    category: "protein",
+    productDetails: {
+      flavours: ["chocolate", "mango"],
     },
-    {
-        productName: "Mass Gainer Elite",
-        brandName: "Optimum Nutrition",
-        category: "gainer",
-        productDetails: {
-            flavours: ["chocolate"]
-        },
-        sizes: {
-            weight: [1000, 2000]
-        },
-        stock: {
-            quantity: 6
-        },
-        price: {
-            productPrice: 3200
-        },
-        images: {
-            displayImage: "/products/gainer.webp",
-            sliderImages: ["/products/gainer.webp", "/products/protein-isolate.jpg"]
-        }
+    sizes: {
+      weight: [500, 1000],
     },
-    {
-        productName: "Pre-Workout Explosion",
-        brandName: "MuscleTech",
-        category: "pre-workout",
-        productDetails: {
-            flavours: ["strawberry"]
-        },
-        sizes: {
-            weight: [500]
-        },
-        stock: {
-            quantity: 10
-        },
-        price: {
-            productPrice: 1800
-        },
-        images: {
-            displayImage: "/products/on-supex-supplement.png",
-            sliderImages: ["/products/creatine.webp", "/products/protein.jpg"]
-        }
+    stock: {
+      quantity: 4,
     },
-    {
-        productName: "Post-Workout Explosion",
-        brandName: "MuscleMadness",
-        category: "post-workout",
-        productDetails: {
-            flavours: ["strawberry"]
-        },
-        sizes: {
-            weight: [500]
-        },
-        stock: {
-            quantity: 10
-        },
-        price: {
-            productPrice: 1800
-        },
-        images: {
-            displayImage: "/products/on-supex-supplement.png",
-            sliderImages: ["/products/creatine.webp", "/products/protein.jpg"]
-        }
+    price: {
+      productPrice: 2500,
+      onSale: true,
     },
-    {
-        productName: "Daily Vitamin Boost",
-        brandName: "GNC",
-        category: "vitamin",
-        productDetails: {
-            flavours: ["unflavoured"]
-        },
-        sizes: {
-            weight: [250, 500]
-        },
-        stock: {
-            quantity: 8
-        },
-        price: {
-            productPrice: 2200
-        },
-        images: {
-            displayImage: "/products/vitamin.webp",
-            sliderImages: ["/products/vitamin.webp", "/products/on-supex-supplement.png"]
-        }
+    images: {
+      displayImage: "/products/protein.jpg",
+      sliderImages: ["/products/protein.jpg", "/products/gainer.webp"],
     },
-    {
-        productName: "VitalGear Compression Shirt",
-        brandName: "VitalGear",
-        category: "active-wear",
-        productDetails: {
-            colors: ["black", "grey"]
-        },
-        sizes: {
-            shirtSize: ["m", "l", "xl"]
-        },
-        stock: {
-            quantity: 12
-        },
-        price: {
-            productPrice: 1500
-        },
-        images: {
-            displayImage: "/products/compression.webp",
-            sliderImages: ["/products/compression.webp", "/products/gainer.webp"]
-        }
+  },
+  {
+    productName: "Mass Gainer Elite",
+    brandName: "Optimum Nutrition",
+    category: "gainer",
+    productDetails: {
+      flavours: ["chocolate"],
     },
-    {
-        productName: "Gold Standard Whey",
-        brandName: "Optimum Nutrition",
-        category: "protein",
-        productDetails: {
-            flavours: ["chocolate", "strawberry"]
-        },
-        sizes: {
-            weight: [500, 1000, 2000]
-        },
-        stock: {
-            quantity: 5
-        },
-        price: {
-            productPrice: 3000,
-            onSale: true
-        },
-        images: {
-            displayImage: "/products/protein-isolate.jpg",
-            sliderImages: ["/products/protein.jpg", "/products/vitamin.webp"]
-        }
+    sizes: {
+      weight: [1000, 2000],
     },
-    {
-        productName: "Isopure Zero Carb",
-        brandName: "Nature's Best",
-        category: "protein",
-        productDetails: {
-            flavours: ["unflavoured"]
-        },
-        sizes: {
-            weight: [1000]
-        },
-        stock: {
-            quantity: 7
-        },
-        price: {
-            productPrice: 3500
-        },
-        images: {
-            displayImage: "/products/protein.jpg",
-            sliderImages: ["/products/on-supex-supplement.png", "/products/creatine.webp"]
-        }
+    stock: {
+      quantity: 6,
     },
-    {
-        productName: "Hydro Whey Platinum",
-        brandName: "Optimum Nutrition",
-        category: "protein",
-        productDetails: {
-            flavours: ["mango"]
-        },
-        sizes: {
-            weight: [500, 2000]
-        },
-        stock: {
-            quantity: 3
-        },
-        price: {
-            productPrice: 4000,
-            onSale: false
-        },
-        images: {
-            displayImage: "/products/protein-isolate.jpg",
-            sliderImages: ["/products/gainer.webp", "/products/protein.jpg"]
-        }
+    price: {
+      productPrice: 3200,
     },
-    {
-        productName: "Dymatize Elite Whey",
-        brandName: "Dymatize",
-        category: "protein",
-        productDetails: {
-            flavours: ["strawberry"]
-        },
-        sizes: {
-            weight: [500, 1000]
-        },
-        stock: {
-            quantity: 9
-        },
-        price: {
-            productPrice: 2800
-        },
-        images: {
-            displayImage: "/products/creatine.webp",
-            sliderImages: ["/products/creatine.webp", "/products/protein-isolate.jpg"]
-        }
+    images: {
+      displayImage: "/products/gainer.webp",
+      sliderImages: ["/products/gainer.webp", "/products/protein-isolate.jpg"],
     },
-    {
-        productName: "NitroTech Whey Gold",
-        brandName: "MuscleTech",
-        category: "protein",
-        productDetails: {
-            flavours: ["chocolate", "mango"]
-        },
-        sizes: {
-            weight: [500, 2000, 4000]
-        },
-        stock: {
-            quantity: 6
-        },
-        price: {
-            productPrice: 4200
-        },
-        images: {
-            displayImage: "/products/protein.jpg",
-            sliderImages: ["/products/protein.jpg", "/products/compression.webp"]
-        }
+  },
+  {
+    productName: "Pre-Workout Explosion",
+    brandName: "MuscleTech",
+    category: "pre-workout",
+    productDetails: {
+      flavours: ["strawberry"],
     },
-    {
-        productName: "Rule 1 R1 Whey Blend",
-        brandName: "Rule 1",
-        category: "protein",
-        productDetails: {
-            flavours: ["chocolate"]
-        },
-        sizes: {
-            weight: [500, 1000, 2000]
-        },
-        stock: {
-            quantity: 10
-        },
-        price: {
-            productPrice: 3400
-        },
-        images: {
-            displayImage: "/products/protein-isolate.jpg",
-            sliderImages: ["/products/on-supex-supplement.png", "/products/protein.jpg"]
-        }
+    sizes: {
+      weight: [500],
     },
-    {
-        productName: "Impact Whey Protein",
-        brandName: "MyProtein",
-        category: "protein",
-        productDetails: {
-            flavours: ["strawberry", "unflavoured"]
-        },
-        sizes: {
-            weight: [500, 1000, 2000]
-        },
-        stock: {
-            quantity: 8
-        },
-        price: {
-            productPrice: 2700
-        },
-        images: {
-            displayImage: "/products/gainer.webp",
-            sliderImages: ["/products/gainer.webp", "/products/vitamin.webp"]
-        }
+    stock: {
+      quantity: 10,
     },
-    {
-        productName: "GNC Pro Performance Whey",
-        brandName: "GNC",
-        category: "protein",
-        productDetails: {
-            flavours: ["mango"]
-        },
-        sizes: {
-            weight: [500, 1000]
-        },
-        stock: {
-            quantity: 12
-        },
-        price: {
-            productPrice: 2600
-        },
-        images: {
-            displayImage: "/products/compression.webp",
-            sliderImages: ["/products/compression.webp", "/products/on-supex-supplement.png"]
-        }
-    }
+    price: {
+      productPrice: 1800,
+    },
+    images: {
+      displayImage: "/products/on-supex-supplement.png",
+      sliderImages: ["/products/creatine.webp", "/products/protein.jpg"],
+    },
+  },
+  {
+    productName: "Post-Workout Explosion",
+    brandName: "MuscleMadness",
+    category: "post-workout",
+    productDetails: {
+      flavours: ["strawberry"],
+    },
+    sizes: {
+      weight: [500],
+    },
+    stock: {
+      quantity: 10,
+    },
+    price: {
+      productPrice: 1800,
+    },
+    images: {
+      displayImage: "/products/on-supex-supplement.png",
+      sliderImages: ["/products/creatine.webp", "/products/protein.jpg"],
+    },
+  },
+  {
+    productName: "Daily Vitamin Boost",
+    brandName: "GNC",
+    category: "vitamin",
+    productDetails: {
+      flavours: ["unflavoured"],
+    },
+    sizes: {
+      weight: [250, 500],
+    },
+    stock: {
+      quantity: 8,
+    },
+    price: {
+      productPrice: 2200,
+    },
+    images: {
+      displayImage: "/products/vitamin.webp",
+      sliderImages: [
+        "/products/vitamin.webp",
+        "/products/on-supex-supplement.png",
+      ],
+    },
+  },
+  {
+    productName: "VitalGear Compression Shirt",
+    brandName: "VitalGear",
+    category: "active-wear",
+    productDetails: {
+      colors: ["black", "grey"],
+    },
+    sizes: {
+      shirtSize: ["m", "l", "xl"],
+    },
+    stock: {
+      quantity: 12,
+    },
+    price: {
+      productPrice: 1500,
+    },
+    images: {
+      displayImage: "/products/compression.webp",
+      sliderImages: ["/products/compression.webp", "/products/gainer.webp"],
+    },
+  },
+  {
+    productName: "Gold Standard Whey",
+    brandName: "Optimum Nutrition",
+    category: "protein",
+    productDetails: {
+      flavours: ["chocolate", "strawberry"],
+    },
+    sizes: {
+      weight: [500, 1000, 2000],
+    },
+    stock: {
+      quantity: 5,
+    },
+    price: {
+      productPrice: 3000,
+      onSale: true,
+    },
+    images: {
+      displayImage: "/products/protein-isolate.jpg",
+      sliderImages: ["/products/protein.jpg", "/products/vitamin.webp"],
+    },
+  },
+  {
+    productName: "Isopure Zero Carb",
+    brandName: "Nature's Best",
+    category: "protein",
+    productDetails: {
+      flavours: ["unflavoured"],
+    },
+    sizes: {
+      weight: [1000],
+    },
+    stock: {
+      quantity: 7,
+    },
+    price: {
+      productPrice: 3500,
+    },
+    images: {
+      displayImage: "/products/protein.jpg",
+      sliderImages: [
+        "/products/on-supex-supplement.png",
+        "/products/creatine.webp",
+      ],
+    },
+  },
+  {
+    productName: "Hydro Whey Platinum",
+    brandName: "Optimum Nutrition",
+    category: "protein",
+    productDetails: {
+      flavours: ["mango"],
+    },
+    sizes: {
+      weight: [500, 2000],
+    },
+    stock: {
+      quantity: 3,
+    },
+    price: {
+      productPrice: 4000,
+      onSale: false,
+    },
+    images: {
+      displayImage: "/products/protein-isolate.jpg",
+      sliderImages: ["/products/gainer.webp", "/products/protein.jpg"],
+    },
+  },
+  {
+    productName: "Dymatize Elite Whey",
+    brandName: "Dymatize",
+    category: "protein",
+    productDetails: {
+      flavours: ["strawberry"],
+    },
+    sizes: {
+      weight: [500, 1000],
+    },
+    stock: {
+      quantity: 9,
+    },
+    price: {
+      productPrice: 2800,
+    },
+    images: {
+      displayImage: "/products/creatine.webp",
+      sliderImages: [
+        "/products/creatine.webp",
+        "/products/protein-isolate.jpg",
+      ],
+    },
+  },
+  {
+    productName: "NitroTech Whey Gold",
+    brandName: "MuscleTech",
+    category: "protein",
+    productDetails: {
+      flavours: ["chocolate", "mango"],
+    },
+    sizes: {
+      weight: [500, 2000, 4000],
+    },
+    stock: {
+      quantity: 6,
+    },
+    price: {
+      productPrice: 4200,
+    },
+    images: {
+      displayImage: "/products/protein.jpg",
+      sliderImages: ["/products/protein.jpg", "/products/compression.webp"],
+    },
+  },
+  {
+    productName: "Rule 1 R1 Whey Blend",
+    brandName: "Rule 1",
+    category: "protein",
+    productDetails: {
+      flavours: ["chocolate"],
+    },
+    sizes: {
+      weight: [500, 1000, 2000],
+    },
+    stock: {
+      quantity: 10,
+    },
+    price: {
+      productPrice: 3400,
+    },
+    images: {
+      displayImage: "/products/protein-isolate.jpg",
+      sliderImages: [
+        "/products/on-supex-supplement.png",
+        "/products/protein.jpg",
+      ],
+    },
+  },
+  {
+    productName: "Impact Whey Protein",
+    brandName: "MyProtein",
+    category: "protein",
+    productDetails: {
+      flavours: ["strawberry", "unflavoured"],
+    },
+    sizes: {
+      weight: [500, 1000, 2000],
+    },
+    stock: {
+      quantity: 8,
+    },
+    price: {
+      productPrice: 2700,
+    },
+    images: {
+      displayImage: "/products/gainer.webp",
+      sliderImages: ["/products/gainer.webp", "/products/vitamin.webp"],
+    },
+  },
+  {
+    productName: "GNC Pro Performance Whey",
+    brandName: "GNC",
+    category: "protein",
+    productDetails: {
+      flavours: ["mango"],
+    },
+    sizes: {
+      weight: [500, 1000],
+    },
+    stock: {
+      quantity: 12,
+    },
+    price: {
+      productPrice: 2600,
+    },
+    images: {
+      displayImage: "/products/compression.webp",
+      sliderImages: [
+        "/products/compression.webp",
+        "/products/on-supex-supplement.png",
+      ],
+    },
+  },
 ];
 
-
-vitalProduct.deleteMany({})
-.then(()=>{
-    console.log('Deleted...');
-})
-.catch(err=>{
+vitalProduct
+  .deleteMany({})
+  .then(() => {
+    console.log("Deleted...");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
-vitalProduct.insertMany(seedProducts) 
-.then(()=>{
+vitalProduct
+  .insertMany(seedProducts)
+  .then(() => {
     console.log("Data added successfully...");
-})
-.catch(err=>{
+  })
+  .catch((err) => {
     console.log("ERROR encountered:");
     console.log(err);
-})
+  });
